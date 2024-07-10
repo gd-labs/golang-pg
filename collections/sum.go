e6f5d3f05196f3e05d851d4c03347abdfd6ec236
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-09 21:05:36 -0300

    feat(collection): add slice operations

--- collections/sum.go
+++ collections/sum.go
@@ -2,8 +2,31 @@ package collections
 
 func Sum(numbers []int) int {
 	sum := 0
-	for _, num := range numbers {
-		sum += num
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(slices ...[]int) []int {
+	var sums []int
+	for _, numbers := range slices {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- collections/sum_test.go
+++ collections/sum_test.go
@@ -1,6 +1,9 @@
 package collections
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -21,6 +24,35 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d, want %d given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// Checks if any two variables are the same.
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, wan %v", got, want)
+		}
+	})
+	t.Run("sum with empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	})
 }
